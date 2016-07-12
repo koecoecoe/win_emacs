7b874013a55816c46c4cf4f46cbec1c225b4c5b5
masaki_hashimoto <masaki_hashimoto>
2016-07-13 02:00:09 +0900

    firstcommit

---
+++ el-get/.status.el
@@ -0,0 +1,60 @@
+((el-get status "installed" recipe
+	 (:name el-get :website "https://github.com/dimitri/el-get#readme" :description "Manage the external elisp bits and pieces you depend upon." :type github :branch "master" :pkgname "dimitri/el-get" :info "." :compile
+		("el-get.*\\.el$" "methods/")
+		:features el-get :post-init
+		(when
+		    (memq 'el-get
+			  (bound-and-true-p package-activated-list))
+		  (message "Deleting melpa bootstrap el-get")
+		  (unless package--initialized
+		    (package-initialize t))
+		  (when
+		      (package-installed-p 'el-get)
+		    (let
+			((feats
+			  (delete-dups
+			   (el-get-package-features
+			    (el-get-elpa-package-directory 'el-get)))))
+		      (el-get-elpa-delete-package 'el-get)
+		      (dolist
+			  (feat feats)
+			(unload-feature feat t))))
+		  (require 'el-get))))
+ (hc-zenburn-theme status "required" recipe nil)
+ (package status "required" recipe
+	  (:name package :description "ELPA implementation (\"package.el\") from Emacs 24" :builtin "24" :type http :url "http://repo.or.cz/w/emacs.git/blob_plain/ba08b24186711eaeb3748f3d1f23e2c2d9ed0d09:/lisp/emacs-lisp/package.el" :shallow nil :features package :post-init
+		 (progn
+		   (let
+		       ((old-package-user-dir
+			 (expand-file-name
+			  (convert-standard-filename
+			   (concat
+			    (file-name-as-directory default-directory)
+			    "elpa")))))
+		     (when
+			 (file-directory-p old-package-user-dir)
+		       (add-to-list 'package-directory-list old-package-user-dir)))
+		   (setq package-archives
+			 (bound-and-true-p package-archives))
+		   (mapc
+		    (lambda
+		      (pa)
+		      (add-to-list 'package-archives pa 'append))
+		    '(("ELPA" . "http://tromey.com/elpa/")
+		      ("melpa" . "http://melpa.org/packages/")
+		      ("gnu" . "http://elpa.gnu.org/packages/")
+		      ("marmalade" . "http://marmalade-repo.org/packages/")
+		      ("SC" . "http://joseito.republika.pl/sunrise-commander/"))))))
+ (plantuml-mode status "installed" recipe
+		(:name plantuml-mode :after nil :description "Major mode for PlantUML." :type github :pkgname "zwz/plantuml-mode" :post-init
+		       (let
+			   ((plantuml-url "http://sourceforge.net/projects/plantuml/files/plantuml.jar/download")
+			    (plantuml-jar
+			     (expand-file-name "plantuml.jar" default-directory)))
+			 (when
+			     (not
+			      (file-exists-p plantuml-jar))
+			   (url-copy-file plantuml-url plantuml-jar)))
+		       :prepare
+		       (setq plantuml-jar-path
+			     (expand-file-name "plantuml.jar" default-directory)))))
---
+++ init - コピー.el
@@ -0,0 +1,205 @@
+;;emacs -q -l ~/path/to/somewhere/init.el
+;;の様な起動で、設定ファイルに干渉すること無く、別の設定ファイルを試す事が出来る
+(when load-file-name
+  (setq user-emacs-directory (file-name-directory load-file-name)))
+
+;;(add-to-list 'default-frame-alist '(font . "HGｺﾞｼｯｸM"))
+(add-to-list 'initial-frame-alist '(font . "HGｺﾞｼｯｸM"))
+
+;;画面設定
+   (add-hook 'term-setup-hook
+               #'(lambda () (w32-send-sys-command ?\xF030)))
+               
+;; 日付時間挿入
+(defun insert-current-daytime()
+  (interactive)
+  (insert (format-time-string "%Y/%m/%d %H:%M:%S \n" (current-time))))
+
+(define-key global-map [f5] `insert-current-daytime)
+
+;; 日付挿入
+(defun insert-current-time()
+  (interactive)
+  (insert (format-time-string "%Y/%m/%d " (current-time))))
+
+(define-key global-map [?\C-;] `insert-current-time)
+
+;;trampの設定（puty）
+(setenv "PATH" (concat "C:\\Users\masaki_hashimoto\Documents\putty-0.60-JP_Y-2007-08-06\plink.exe" ";" (getenv "PATH")))
+
+;;フォント設定
+
+(set-fontset-font "fontset-standard"
+   		  'ascii
+   		  (font-spec :family "HGｺﾞｼｯｸM" :size 11) nil 'prepend)  ;; ここでサイズを指定
+   		  
+;; org-plantuml-jar-path は plantuml.jar へのパス
+(setq org-plantuml-jar-path "~/plantuml/plantuml.jar")
+(defun org-mode-init ()
+  (org-babel-do-load-languages
+   'org-babel-load-languages
+   (add-to-list 'org-babel-load-languages '(plantuml . t))))
+(add-hook 'org-mode-hook 'org-mode-init)
+
+;;active Org-babel languages※org-modeでditaaを使う※jditaaに設定済み
+;; active Org-babel languages
+(setq org-ditaa-jar-path "~/ditaa/ditaa09/jditaa.jar")
+
+;; コードを評価するとき尋ねない
+(setq org-confirm-babel-evaluate nil)
+;; 有効にする言語 デフォルトではelispのみ
+(org-babel-do-load-languages
+ 'org-babel-load-languages
+ '((ditaa . t)
+   (dot . t)))
+
+(setq org-export-html-coding-system 'utf-8)
+
+;;picture-mode-initをロードする
+(add-hook 'picture-mode-hook 'picture-mode-init)
+(autoload 'picture-mode-init "picture-init")
+
+;;C-hでバックスペース
+(global-set-key "\C-h" 'delete-backward-char)
+;packages
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/#/"))
+(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
+(add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/"))
+(package-initialize)
+
+
+(add-to-list 'load-path ".emacs.d/")
+;;======================================================================
+;; 言語・文字コード関連の設定
+;;======================================================================
+
+(set-language-environment "Japanese")
+(set-terminal-coding-system 'utf-8)
+(set-keyboard-coding-system 'utf-8)
+(set-buffer-file-coding-system 'utf-8)
+(setq default-buffer-file-coding-system 'utf-8)
+(prefer-coding-system 'utf-8)
+(set-default-coding-systems 'utf-8)
+(setq file-name-coding-system 'utf-8)
+;;
+;;======================================================================
+;; Mozc
+;; CTRL-\で入力モード切替え
+;;======================================================================
+;(load-file "/usr/share/emacs/site-lisp/emacs-mozc/mozc.el")
+;(require 'mozc)
+;(setq default-input-method "japanese-mozc")
+;(setq mozc-candidate-style 'overlay)
+;;
+;;=======================================================================
+;; Misc
+;;=======================================================================
+(setq frame-title-format ; フレームのタイトル指定
+      (concat "%b - emacs@" system-name))
+
+(display-time) ;;時計を表示
+(show-paren-mode t)
+(setq inhibit-startup-message t) ;
+(setq initial-scratch-message nil) ;スタートアップスクリーン抑制
+(setq comment-style 'multiline) ;ボックスコメント
+;;
+
+;;=======================================================================
+;; ~/.emacs.d/elpa/ にパスを通す
+;;=======================================================================
+(add-to-list 'load-path (expand-file-name "~/.emacs.d/elpa/"))
+;;
+;;=======================================================================
+;;packages
+;;=======================================================================
+					;packages
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/#/"))
+(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
+(add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/"))
+(package-initialize)
+;;
+;;=======================================================================
+;;web-mode
+;;=======================================================================
+					;web-mode
+(require 'web-mode)
+;;; 適用する拡張子
+(add-to-list 'auto-mode-alist '("\\.phtml$" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.tpl\\.php$" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.jsp$" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.as[cp]x$" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.erb$" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.html?$" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.ctp?$" . web-mode))
+
+(defun web-mode-hook ()
+  "Hooks for Web mode."
+  ;; 変更日時の自動修正
+  (setq time-stamp-line-limit -200)
+  (if (not (memq 'time-stamp write-file-hooks))
+      (setq write-file-hooks
+	    (cons 'time-stamp write-file-hooks)))
+  (setq time-stamp-format " %3a %3b %02d %02H:%02M:%02S %:y %Z")
+  (setq time-stamp-start "Last modified:")
+  (setq time-stamp-end "$")
+  ;; web-modeの設定
+  (setq web-mode-markup-indent-offset 2) ;; html indent
+  (setq web-mode-css-indent-offset 2) ;; css indent
+  (setq web-mode-code-indent-offset 2) ;; script indent(js,php,etc..)
+  ;; htmlの内容をインデント
+  ;; TEXTAREA等の中身をインデントすると副作用が起こったりするので
+  ;; デフォルトではインデントしない
+  ;;(setq web-mode-indent-style 2)
+  ;; コメントのスタイル
+  ;; 1:htmlのコメントスタイル(default)
+  ;; 2:テンプレートエンジンのコメントスタイル
+  ;; (Ex. {# django comment #},{* smarty comment *},{{-- blade comment --}})
+  (setq web-mode-comment-style 2)
+  ;; 終了タグの自動補完をしない
+  ;;(setq web-mode-disable-auto-pairing t)
+  ;; color:#ff0000;等とした場合に指定した色をbgに表示しない
+  ;;(setq web-mode-disable-css-colorization t)
+  ;;css,js,php,etc..の範囲をbg色で表示
+  ;; (setq web-mode-enable-block-faces t)
+  ;; (custom-set-faces
+  ;; '(web-mode-server-face
+  ;; ((t (:background "grey")))) ; template Blockの背景色
+  ;; '(web-mode-css-face
+  ;; ((t (:background "grey18")))) ; CSS Blockの背景色
+  ;; '(web-mode-javascript-face
+  ;; ((t (:background "grey36")))) ; javascript Blockの背景色
+  ;; )
+  ;;(setq web-mode-enable-heredoc-fontification t)
+  )
+
+;; 色の設定
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(web-mode-comment-face ((t (:foreground "#D30628")))) ;コメント
+ '(web-mode-css-at-rule-face ((t (:foreground "#FF7F00")))) ;CSSタグ
+ '(web-mode-css-pseudo-class-face ((t (:foreground "#FF7F00")))) ;CSS擬似クラス
+ '(web-mode-css-rule-face ((t (:foreground "#A0D8EF")))) ;CSSタグ
+ '(web-mode-doctype-face ((t (:foreground "#82AE46")))) ;docutype
+ '(web-mode-html-attr-name-face ((t (:foreground "#C97586")))) ;属性名など
+ '(web-mode-html-attr-value-face ((t (:foreground "#82AE46")))) ;属性値
+ '(web-mode-html-tag-face ((t (:foreground "##4682ae" :weight bold)))) ;要素名
+ '(web-mode-server-comment-face ((t (:foreground "#D9333F"))))) ;コメント
+(add-hook 'web-mode-hook 'web-mode-hook)
+
+;;=======================================================================
+;;php-mode
+;;=======================================================================
+(autoload 'php-mode "php-mode")
+(setq auto-mode-alist
+      (cons '("\\.php\\'" . php-mode) auto-mode-alist))
+(setq php-mode-force-pear t)
+(add-hook 'php-mode-hook
+	  '(lambda ()
+	     (setq php-manual-path "/usr/local/share/php/doc/html")
+	     (setq php-manual-url "http://www.phppro.jp/phpmanual/")))
